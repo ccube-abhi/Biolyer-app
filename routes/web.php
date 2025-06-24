14e9096e657d568ebaf4ccbfd4571ba0f5d6432b
Abhishek Bari <abhishekbari@Abhisheks-MacBook-Pro.local>
2025-06-24 16:07:44 +0530

    Redis setup with Docker

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+use Illuminate\Support\Facades\Redis;
+
+Route::get('/redis-test', function () {
+    Redis::set('name', 'Abhishek');
+    return Redis::get('name'); // Should return "Abhishek"
+});
