19a444e32bbce20584f7bab9416d3c4893392897
Abhishek Bari <abhishekbari@Abhisheks-MacBook-Pro.local>
2025-07-14 11:08:17 +0530

    access data using model

--- app/Models/blog.php
+++ app/Models/blog.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Blog extends Model
 {
+    protected $table = 'blogs';
+
     protected $connection = 'mysql_second';
     protected $fillable = [
         'user_id', 'title', 'body'
